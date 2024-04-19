c1d5e03ca89bd849bd92793180480abed0745503
Sasha0411 <162753245+Sasha0411@users.noreply.github.com>
2024-04-19 18:07:01 +0300

    Create Task_5.cs

---
+++ Task_5.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace йуцй
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            double step = 0.2;
+
+            // Вивід заголовка таблиці
+            Console.WriteLine("x\ty = sin(x) / ln(x + 4)");
+            Console.WriteLine("--------------------------");
+
+            // Розрахунок та вивід значень функції
+            for (x = -2; x <= 5; x += step)
+            {
+                if (x == 0)
+                {
+                    y = double.NaN; // Функція не визначена при x = 0
+                }
+                else
+                {
+                    y = Math.Sin(x) / Math.Log(x + 4);
+                }
+
+                Console.WriteLine("{0:F2}\t{1:F4}", x, y);
+
+            }   
+        }
+    }
+}
